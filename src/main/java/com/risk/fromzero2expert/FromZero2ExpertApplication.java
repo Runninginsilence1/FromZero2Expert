823b7882fac1bce308860a35fe6bc11558d0d477
Runninginsilence <runninginsilence0@gmail.com>
2022-09-28 12:31:41 +0800

    Initialze the repository, then check the 'dev' branch please

---
+++ src/main/java/com/risk/fromzero2expert/FromZero2ExpertApplication.java
@@ -0,0 +1,13 @@
+package com.risk.fromzero2expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromZero2ExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FromZero2ExpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/risk/fromzero2expert/FromZero2ExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risk.fromzero2expert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromZero2ExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
